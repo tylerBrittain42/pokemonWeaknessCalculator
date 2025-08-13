6a49305a55ba5f1ce6d52ba202e1c810266456c2
Tyler Brittain <tbrittain100@gmail.com>
2025-08-13 16:32:26 -0700

    feat: complete getType

--- pkg/pokeWrapper/pokeWrapper.go
+++ pkg/pokeWrapper/pokeWrapper.go
@@ -3,10 +3,12 @@ package pokewrapper
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"net/http"
 	"strings"
 )
 
+// Sanitizes the name of a pokemon
 func cleanInput(name string) (string, error) {
 	trimmed := strings.TrimSpace(name)
 	if trimmed == "" {
@@ -18,15 +20,39 @@ func cleanInput(name string) (string, error) {
 
 }
 
+// Returns the type of the pokemon
+//
+// Assumes that the name has been sanitized
+func getType(name string) ([]string, error) {
+	var specPokemon Pokemon
+	types := []string{}
+
+	url := "https://" + domain + "pokemon/" + name
+	resp, err := http.Get(url)
+	if err != nil {
+		return []string{}, nil
+	}
+
+	if resp.StatusCode != 200 {
+		return []string{}, fmt.Errorf("unable to find match for: %s", name)
+	}
+
+	defer resp.Body.Close()
+	decoder := json.NewDecoder(resp.Body)
+	err = decoder.Decode((&specPokemon))
+	if err != nil {
+		return []string{}, err
+	}
+
+	types = append(types, specPokemon.Types[0].Type.Name)
+	if len(specPokemon.Types) > 1 {
+		types = append(types, specPokemon.Types[1].Type.Name)
+	}
+
+	return types, nil
+
 }
 
 func Foo() {
-	res, err := http.Get("https://google.com")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer res.Body.Close()
-	data, _ := io.ReadAll(res.Body)
-	fmt.Println(string(data))
+	getType("bulbasaur")
 }
--- pkg/pokeWrapper/pokewrapper_test.go
+++ pkg/pokeWrapper/pokewrapper_test.go
@@ -10,47 +10,56 @@ func TestCleanInput(t *testing.T) {
 		inputPokemon    string
 		expectedPokemon string
 		shouldError     bool
+		errorMessage    string
 	}{
 		{
 			caseName:        "base case",
 			inputPokemon:    "turtwig",
 			expectedPokemon: "turtwig",
 			shouldError:     false,
+			errorMessage:    "",
 		}, {
 			caseName:        "assorted caps",
 			inputPokemon:    "BuLbASaur",
 			expectedPokemon: "bulbasaur",
 			shouldError:     false,
+			errorMessage:    "",
 		}, {
 			caseName:        "multiple word name without dashes",
 			inputPokemon:    "iron hands",
 			expectedPokemon: "iron-hands",
 			shouldError:     false,
+			errorMessage:    "",
 		}, {
 			caseName:        "multiple world name using dashes",
 			inputPokemon:    "iron-hands",
 			expectedPokemon: "iron-hands",
 			shouldError:     false,
+			errorMessage:    "",
 		}, {
 			caseName:        "form name with spaces",
 			inputPokemon:    "sandslash alola",
 			expectedPokemon: "sandslash-alola",
 			shouldError:     false,
+			errorMessage:    "",
 		}, {
 			caseName:        "form name with dashes",
 			inputPokemon:    "sandslash alola",
 			expectedPokemon: "sandslash-alola",
 			shouldError:     false,
+			errorMessage:    "",
 		}, {
 			caseName:        "nothing given",
 			inputPokemon:    "",
 			expectedPokemon: "",
 			shouldError:     true,
+			errorMessage:    "empty string was given as name",
 		}, {
 			caseName:        "only whitespace given",
 			inputPokemon:    "     ",
 			expectedPokemon: "",
 			shouldError:     true,
+			errorMessage:    "empty string was given as name",
 		},
 	}
 
@@ -65,6 +74,8 @@ func TestCleanInput(t *testing.T) {
 			} else {
 				if err == nil {
 					t.Errorf("%s %s did not error out", tt.caseName, tt.inputPokemon)
+				} else if err.Error() != tt.errorMessage {
+					t.Errorf("Got %s, expected %s", err.Error(), tt.errorMessage)
 				}
 
 			}
@@ -79,20 +90,44 @@ func TestGetType(t *testing.T) {
 	cases := []struct {
 		caseName      string
 		inputPokemon  string
-		expectedTypes [2]string
+		expectedTypes []string
 		shouldError   bool
+		errorMessage  string
 	}{
 		{
 			caseName:      "single type(base case)",
 			inputPokemon:  "turtwig",
-			expectedTypes: [2]string{"grass", "none"},
+			expectedTypes: []string{"grass"},
+			shouldError:   false,
+			errorMessage:  "",
+		}, {
+			caseName:      "dual type",
+			inputPokemon:  "bulbasaur",
+			expectedTypes: []string{"grass", "poison"},
+			shouldError:   false,
+			errorMessage:  "",
+		}, {
+			caseName:      "multiple word name without dashes(invalid)",
+			inputPokemon:  "iron hands",
+			expectedTypes: []string{},
+			shouldError:   true,
+			errorMessage:  "unable to find match for: iron hands",
+		}, {
+			caseName:      "multiple world name using dashes",
+			inputPokemon:  "iron-hands",
+			expectedTypes: []string{"fighting", "electric"},
+			shouldError:   false,
+			errorMessage:  "",
+		}, {
+			caseName:      "alolan form",
+			inputPokemon:  "sandslash-alola",
+			expectedTypes: []string{"ice", "steel"},
 			shouldError:   false,
 		},
 		// cases
-		// dual type
-		// multiple regions, think the snow sandslash
+
+		// multiple regions, think the snow sandslash <--consider using generation for handling these casses
 		//no result found
-		//multiple words in name
 		// mega
 		// type changes depending on generation(Azurill or other fairy)
 	}
@@ -101,8 +136,15 @@ func TestGetType(t *testing.T) {
 		t.Run(tt.caseName, func(t *testing.T) {
 			output, err := getType(tt.inputPokemon)
 			if !tt.shouldError {
-				if output[0] != tt.expectedTypes[0] && output[1] != tt.expectedTypes[1] {
-					t.Errorf("for %s, %s-%s != %s-%s", tt.inputPokemon, output[0], output[1], tt.expectedTypes[0], tt.expectedTypes[1])
+				if len(output) != len(tt.expectedTypes) {
+					t.Errorf("for %s, the improper number of types were listed", tt.inputPokemon)
+				}
+				if output[0] != tt.expectedTypes[0] {
+					t.Errorf("for %s, %s != %s", tt.inputPokemon, output[0], tt.expectedTypes[0])
+				}
+
+				if len(output) == 2 && output[1] != tt.expectedTypes[1] {
+					t.Errorf("for %s, %s-%s != (expected) %s-%s", tt.inputPokemon, output[0], output[1], tt.expectedTypes[0], tt.expectedTypes[1])
 				}
 			} else {
 				if err == nil {
